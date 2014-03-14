888ded271dcb0a1a29aac07ae69cc4afda3735c6
Felix Schulze <fschulze@autoscout24.com>
2014-03-14 11:00:47 +0100

    Initial commit

---
+++ src/androidTest/java/de/felixschulze/spoon_test/app/MainActivityTest.java
@@ -0,0 +1,12 @@
+package de.felixschulze.spoon_test.app;
+
+import android.test.InstrumentationTestCase;
+
+public class MainActivityTest extends InstrumentationTestCase {
+
+    public void testExample() {
+
+        assertNotNull("sample");
+
+    }
+}
